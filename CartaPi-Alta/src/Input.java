400ff844de8f9822bbfb52c64ad0970340c17aa0
andrewzenfoo <andrewzen13@gmail.com>
2016-05-17 20:17:44 +0200

    Commented parts to be fixed

--- CartaPi-Alta/src/Input.java
+++ CartaPi-Alta/src/Input.java
@@ -1,10 +1,9 @@
-
 public class Input {
 
-public static void creaPartita() {
-    Giocatore giocatore = new Giocatore("", 0);
+    public static void creaPartita() {
+        Giocatore giocatore = new Giocatore("", 0);
 
-    do {
+
         System.out.println("Benvenuto al gioco!");
         System.out.print("Come ti chiami? ");
         String nome = IO.input();
@@ -13,35 +12,41 @@ public static void creaPartita() {
         giocatore.setNome(nome);
         giocatore.setSoldi(soldi);
 
-        int scelta;
-        System.out.printf("Ciao %s!", giocatore.getNome());
-        System.out.println("Cosa vuoi fare?");
-        System.out.println("1) Gioca");
-        System.out.println("2) Esci");
-        System.out.print(">> ");
+        do {
+            int scelta;
+            System.out.printf("\nCiao %s!\n", giocatore.getNome());
+            System.out.println("Cosa vuoi fare?");
+            System.out.println("1) Gioca");
+            System.out.println("2) Esci");
+            System.out.print(">> ");
 
-        scelta = IO.inputInt();
+            scelta = IO.inputInt();
 
-        switch (scelta) {
-            case 1:
-                Mazzo mazzo = new Mazzo();
+            switch (scelta) {
+                case 1:
+                    Mazzo mazzo = new Mazzo();
 
-                if (!mazzo.carteUguali()) {
-                    System.out.print("Scegli quanto scommettere: ");
-                    int scommessa = IO.inputInt();
-                    giocatore.scommetti(scommessa);
-                        if (mazzo.confrontaCarte()) {
-                            System.out.println("Complimenti, hai vinto");
-                            giocatore.gestioneDenaro(mazzo.confrontaCarte(), scommessa);
-                        }
-                }
-                System.out.println("Le carte estratte sono uguali :(");
-                break;
-            case 2:
-                break;
+                    if (!mazzo.carteUguali()) {
+                        System.out.print("Scegli quanto scommettere: ");
+                        int scommessa = IO.inputInt();      //Inserire condizione per controllare l'importo scommesso
+                        giocatore.scommetti(scommessa);
+                            if (mazzo.confrontaCarte()) {
+                                System.out.println("Complimenti, hai vinto");
+                                giocatore.gestioneDenaro(mazzo.confrontaCarte(), scommessa);
+                            }
+                            else {
+                                System.out.println("Oh no, hai perso! :(");
+                            }
+                    }
+                    else {
+                        System.out.println("Le carte estratte sono uguali :(");
+                    }
+                    break;
+                case 2:
+                    break;
+            }
+                    //INSERIRE MESSAGGIO IN CASO DI FINE SOLDI 
         }
+        while (giocatore.getSoldi()!=0);
     }
-    while (giocatore.getSoldi!=0)
-}
-
 }
